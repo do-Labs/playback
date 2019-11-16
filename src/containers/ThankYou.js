3d6ed5b8c7188259c194ad00b6a9abd7e40ca1e7
Humdan <humdan.b@gmail.com>
2019-11-15 19:34:47 -0600

    added icons

--- src/containers/ThankYou.js
+++ src/containers/ThankYou.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {
-    Container
+    Button,
+    Container,
+    Icon,
 } from "semantic-ui-react";
 import {Logo} from "../components/index";
 
@@ -16,11 +18,19 @@ export default class ThankYou extends Component {
                 <Logo />
                 <h1>Thank You for your feedback!!</h1>
                 <p>Let us know if you have any comments about the Playback App</p>
-                <a href="https://playback.herokuapp.com/register">Sign Up</a>
-                <br/>
-                <a href="https://playback.dolabs.io">Learn More</a>
-                <br/>
-                <a href="mailto:humdan@dolabs.io">Contact Us</a>
+                <div>
+                    <center>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <div><a href="https://playback.herokuapp.com/register"><Icon name="edit large"/>Sign Up</a></div>
+                        <br/>
+                        <div><a href="https://playback.dolabs.io"><Icon name="folder open outline large"/>Learn More</a></div>
+                        <br/>
+                        <div><a href="mailto:humdan@dolabs.io"><Icon name="chat large"/>Contact Us</a></div>
+                    </center>
+                </div>
             </Container>
         );
     }
